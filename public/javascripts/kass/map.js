27671393f83d999b40788d26c404ebc37ff22274
Lin He <he9lin@gmail.com>
2011-12-20 18:34:54 -0800

    More

--- public/javascripts/kass/map.js
+++ public/javascripts/kass/map.js
@@ -1,10 +1,8 @@
 (function() {
   var Map, _ref;
-
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   Map = (function() {
-
     function Map() {}
-
     Map.prototype.render = function(collection) {
       this.map = new BMap.Map('map');
       this.map.centerAndZoom(new BMap.Point(121.491, 31.233), 11);
@@ -12,17 +10,14 @@
         anchor: BMAP_ANCHOR_TOP_RIGHT
       }));
     };
-
     Map.prototype.focus = function(lat, lng, content) {
       var point;
       point = new BMap.Point(lat, lng);
       this.showInfoWindow(content, point);
       return this.map.panTo(point);
     };
-
     Map.prototype.addMarker = function(lat, lng, content) {
-      var marker, myIcon, point,
-        _this = this;
+      var marker, myIcon, point;
       point = lng === void 0 ? lat : new BMap.Point(lat, lng);
       myIcon = new BMap.Icon("marker.png", new BMap.Size(48, 66), {
         offset: new BMap.Size(10, 25),
@@ -31,13 +26,12 @@
       marker = new BMap.Marker(point, {
         icon: myIcon
       });
-      marker.addEventListener("click", function(event) {
-        return _this.focus(lat, lng, content);
-      });
+      marker.addEventListener("click", __bind(function(event) {
+        return this.focus(lat, lng, content);
+      }, this));
       this.map.addOverlay(marker);
       return marker;
     };
-
     Map.prototype.showInfoWindow = function(content, lat, lng) {
       var point, w;
       point = lng === void 0 ? lat : new BMap.Point(lat, lng);
@@ -48,13 +42,8 @@
       this.currentOverlay = w;
       return this.map.addOverlay(this.currentOverlay);
     };
-
     return Map;
-
   })();
-
   this.app = (_ref = window.app) != null ? _ref : {};
-
   this.app.Map = new Map;
-
 }).call(this);
